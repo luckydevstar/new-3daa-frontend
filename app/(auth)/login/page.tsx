8e91e63abd4beef013f683e2cb1d6746225074ff
john <johnezekiele30@gmail.com>
2026-01-15 07:36:04 -0600

    add structure for login and sign up page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <h1 className="text-2xl font-black">This is Login page</h1>
+    )
+}
+
+export default Login;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+const Signup = () => {
+    return (
+        <h1 className="text-2xl font-black">This is Signup page</h1>
+    )
+}
+
+export default Signup;
